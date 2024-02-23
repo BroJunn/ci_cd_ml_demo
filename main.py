08d5e066beef7b80a861b4947b832ebe4a4d9d65
王誉钧 <yujun_wang@wangyujundeMacBook-Pro.local>
2024-02-23 14:03:25 +0100

    add PR action and main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import datetime
+
+def main():
+    print("The main function in py file is executed.")
+
+    datetime = datetime.datetime.now()
+    print("current time: ", datetime)
+
+if __name__ == "__main__":
+    main()
